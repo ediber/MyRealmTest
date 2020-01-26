86da2c72ab736b9dfafbb7c7a83dd3e92cece09e
edib <edibersh@gmail.com>
2020-01-26 13:25:45 +0200

    added working read and write examples

--- app/src/main/java/com/e/myrealmtest/MainActivity.java
+++ app/src/main/java/com/e/myrealmtest/MainActivity.java
@@ -1,7 +1,10 @@
 package com.e.myrealmtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.realm.OrderedCollectionChangeSet;
+import io.realm.OrderedRealmCollectionChangeListener;
 import io.realm.Realm;
+import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
 
 import android.os.Bundle;
@@ -9,12 +12,17 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RealmResults<Dog> puppies;
+    private Person person;
+    private Realm realm;
+    private Dog managedDog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.create).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Use them like regular java objects
@@ -22,25 +30,70 @@ public class MainActivity extends AppCompatActivity {
                 dog.setName("Rex");
                 dog.setAge(1);
 
-// Initialize Realm (just once per application)
-                Realm.init(getApplicationContext());
-
-// Get a Realm instance for this thread
-                Realm realm = Realm.getDefaultInstance();
-
-// Query Realm for all dogs younger than 2 years old
-                final RealmResults<Dog> puppies = realm.where(Dog.class).lessThan("age", 2).findAll();
-                puppies.size(); // => 0 because no dogs have been added to the Realm yet
-
 // Persist your data in a transaction
                 realm.beginTransaction();
-                final Dog managedDog = realm.copyToRealm(dog); // Persist unmanaged objects
-                Person person = realm.createObject(Person.class); // Create managed objects directly
+                managedDog = realm.copyToRealm(dog); // Persist unmanaged objects
+                person = realm.createObject(Person.class); // Create managed objects directly
+                //     Person person = realm.createObject(Person.class, UUID.randomUUID().toString());
+                person.getDogs().add(managedDog);
+                realm.commitTransaction();
+
+            }
+        });
+
+        findViewById(R.id.read).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                person = realm.where(Person.class).findFirst();
+            }
+        });
+
+        findViewById(R.id.write).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                realm.beginTransaction();
+
+                // Use them like regular java objects
+                Dog dog = new Dog();
+                dog.setName("Rex");
+                dog.setAge(1);
+                managedDog = realm.copyToRealm(dog); // Persist unmanaged objects
+
+                person = realm.where(Person.class).findFirst();
                 person.getDogs().add(managedDog);
                 realm.commitTransaction();
             }
         });
 
+        findViewById(R.id.init_realm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Initialize Realm (just once per application)
+                Realm.init(getApplicationContext());
+
+// Get a Realm instance for this thread
+                realm = Realm.getDefaultInstance();
+            }
+        });
+
+        findViewById(R.id.puppies_example).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Query Realm for all dogs younger than 2 years old
+                puppies = realm.where(Dog.class).lessThan("age", 2).findAll();
+                puppies.size(); // => 0 because no dogs have been added to the Realm yet
+
+
+                // Listeners will be notified when data changes
+                puppies.addChangeListener(new OrderedRealmCollectionChangeListener<RealmResults<Dog>>() {
+                    @Override
+                    public void onChange(RealmResults<Dog> results, OrderedCollectionChangeSet changeSet) {
+                        // Query results are updated in real time with fine grained notifications.
+                        changeSet.getInsertions(); // => [0] is added.
+                    }
+                });
+            }
+        });
 
     }
 }
--- app/src/main/java/com/e/myrealmtest/Person.java
+++ app/src/main/java/com/e/myrealmtest/Person.java
@@ -5,7 +5,7 @@ import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
 
 public class Person extends RealmObject {
-    @PrimaryKey
+   // @PrimaryKey
     private long id;
     private String name;
     private RealmList<Dog> dogs; // Declare one-to-many relationships
